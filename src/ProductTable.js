a280c47fcc6d860a872e80680ba25b3ba8e2fa15
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-17 13:34:27 -0400

    Create ProductTable.js

---
+++ src/ProductTable.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductCategoryRow from './ProductCategoryRow'
+import ProductRow from './ProductRow'
+
+const ProductTable = () => {
+  return (
+    <div>
+      ProductTable
+      <ProductCategoryRow />
+      <ProductRow />
+    </div>
+  )
+}
+
+export default ProductTable
