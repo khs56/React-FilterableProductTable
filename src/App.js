f9b2acb0984ec25559045609524c5cd81f8ef278
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-17 13:49:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import SearchBar from './SearchBar'
-import ProductTable from './ProductTable'
+import products from './products'
+import FilterableProductTable from './FilterableProductTable'
+
 
 export default function App() {
   return (
     <FilterableProductTable products={products} />
-  );
+  )
 }
-
-const products = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-];
