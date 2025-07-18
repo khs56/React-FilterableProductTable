63f25022b443481273a9c3580176346761906fbe
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-18 15:02:52 -0400

    Update SearchBar.js

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const SearchBar = () => {
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) => {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        value={filterText} placeholder="Search..."
+        onChange={(e) => { onFilterTextChange(e.target.value) }} />
       <label>
-        <input type="checkbox" /> {' '} Only show products in stock
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => { onInStockOnlyChange(e.target.checked) }} />
+        {' '}
+        Only show products in stock
       </label>
     </form>
   )
