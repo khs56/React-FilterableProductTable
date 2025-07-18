95b9b85a174d18b257f45d40dd827c61bac549e7
Vishal M <48333506+khs56@users.noreply.github.com>
2025-07-18 15:02:45 -0400

    Update FilterableProductTable.js

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar'
 import ProductTable from './ProductTable'
 
 const FilterableProductTable = ({ products }) => {
+  const [filterText, setFilterText] = useState('fruit')
+  const [inStockOnly, setInStockOnly] = useState(false)
+
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        products={products}
+      />
     </div>
   )
 }
